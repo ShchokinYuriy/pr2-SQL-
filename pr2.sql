7b3a92f27b38e919b24a5fa0ca421beb06ee3086
ShchokinYuriy <st7363087@stud.duikt.edu.ua>
2024-10-09 17:18:48 +0300

    Update pr2.sql

--- pr2.sql
+++ pr2.sql
@@ -2,13 +2,10 @@ SELECT *
 FROM pr2.users
 ORDER BY LEFT(User_name, 1);
 
--- Зміна роздільника для створення процедури
 DELIMITER //
 
--- Видалення існуючої процедури, якщо така є
 DROP PROCEDURE IF EXISTS GetEvenIDs;
 
--- Створення нової процедури
 CREATE PROCEDURE GetEvenIDs()
 BEGIN
     SELECT *
@@ -16,8 +13,6 @@ BEGIN
     WHERE MOD(id, 2) = 0; -- Використовується MOD для визначення парних id
 END //
 
--- Повернення до стандартного роздільника
 DELIMITER ;
 
--- Виклик процедури
 CALL GetEvenIDs();
